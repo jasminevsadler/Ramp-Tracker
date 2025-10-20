48f8cf768490f13231bd8c02e9d9821ba8637e1e
jasminevsadler <Dr.Jasmine.Sadler@gmail.com>
2025-10-20 00:54:35 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -6,22 +6,5 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-}
-<script>
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function (reg) { console.log('SW registered', reg); })
-      .catch(function (err) { console.error('SW registration failed', err); });
-  });
-}
-</script>
