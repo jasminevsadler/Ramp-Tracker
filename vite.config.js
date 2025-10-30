54436591664067d31fbe4ccfd97d02cd828c2a79
jasminevsadler <Dr.Jasmine.Sadler@gmail.com>
2025-10-30 07:56:31 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',              // make sure it uses the root for index.html
   build: {
-    outDir: 'dist',       // where Vercel expects the final files
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
