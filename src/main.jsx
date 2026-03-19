8e7a8aaf1c5482238f416306175658fdcd913dd4
jasminevsadler <Dr.Jasmine.Sadler@gmail.com>
2026-03-19 13:42:22 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 );
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW error", err));
   });
 }
