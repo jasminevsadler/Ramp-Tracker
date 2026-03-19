b0ab51d42b48a157f12ebdb4f66ab8d8b91710c1
jasminevsadler <Dr.Jasmine.Sadler@gmail.com>
2026-03-19 12:06:13 -0400

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>RaMP It Up!</h1>
-      <p>Fresh start. Clean build. Ready to grow.</p>
-    </div>
-  );
-}
